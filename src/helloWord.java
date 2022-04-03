f4f162e10c99e721b5c24a09861c84e70ac617c1
Mehmet ata cebe <atacebe014@gmail.com>
2022-04-03 12:01:10 +0200

    initial commet

--- src/helloWord.java
+++ src/helloWord.java
@@ -3,5 +3,6 @@ package src;
 public class helloWord {
     public static void main(String[] args) {
         System.out.println("hello word");
+        // doing some work here(you can type anything you want)
     }
 }
